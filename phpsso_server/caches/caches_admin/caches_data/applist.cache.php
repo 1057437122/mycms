0abb6a08f694759fb7adc8090e0f32ddd0806b80
whitelee <shiqiba7@163.com>
2014-08-28 23:09:06 +0800

    initiaize cms

---
+++ phpsso_server/caches/caches_admin/caches_data/applist.cache.php
@@ -0,0 +1,16 @@
+<?php
+return array (
+  1 => 
+  array (
+    'appid' => '1',
+    'type' => 'phpcms_v9',
+    'name' => 'phpcms v9',
+    'url' => 'http://localhost/phpcms/',
+    'authkey' => '80AXgg14AceNqsUnrSr90hiLMLrVkAoP',
+    'ip' => '',
+    'apifilename' => 'api.php?op=phpsso',
+    'charset' => 'utf-8',
+    'synlogin' => '1',
+  ),
+);
+?>
